c871c5eedaa62226e38eee142d67bee510239f7b
HieuNX <xuanbachrtc@gmail.com>
2020-10-22 17:08:14 +0700

    2

--- webapp/Banhang/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Cart, Product, Custumer
-
-# Register your models here.
-admin.site.register(Cart)
-admin.site.register(Product)
-admin.site.register(Custumer)
--- webapp/Banhang/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BanhangConfig(AppConfig):
-    name = 'Banhang'
